082664bfdc904447ab6add4a425457d769335b73
Florian Pilz <fp@gocept.com>
2014-09-18 16:33:41 +0200

    Move send into Client class and separate code to retrieve color from result to add a method to retrieve sound from result later on.

--- client.py
+++ client.py
@@ -1,12 +1,6 @@
 import requests
 
 
-def send(connection, led_number, red, green, blue):
-    message = "LED%02d%03d%03d%03d\n" % (led_number, red, green, blue)
-    connection.write(message)
-    connection.flushInput()
-
-
 class Client(object):
 
     def __init__(self, connection, baseurl, projects, user=None, passwd=None):
@@ -16,33 +10,43 @@ class Client(object):
         self.user = user
         self.passwd = passwd
 
-    def get_color_for_project(self, project):
+    def get_state_for_project(self, project):
+        """Implement in concrete client."""
+        raise NotImplementedError()
+
+    def get_color_for_state(self, state):
         """Implement in concrete client."""
         raise NotImplementedError()
 
     def update(self):
         for project, led_number in self.projects.items():
-            red, green, blue = self.get_color_for_project(project)
-            print("{}: {} {} {} (LED {})".format(
-                project, red, green, blue, led_number))
-            send(self.connection, led_number, red, green, blue)
+            state = self.get_state_for_project(project)
+            print("{}: {} (LED {})".format(project, state, led_number))
+            self.send(self.connection, led_number, state)
+
+    def send(self, connection, led_number, state):
+        red, green, blue = self.get_color_for_state(state)
+        message = "LED%02d%03d%03d%03d\n" % (led_number, red, green, blue)
+        connection.write(message)
+        connection.flushInput()
 
 
 class JenkinsClient(Client):
 
-    def get_color_for_project(self, project):
+    def get_state_for_project(self, project):
         url = "{}/job/{}/api/json".format(self.baseurl, project)
         if self.user and self.passwd:
             response = requests.get(url, auth=(self.user, self.passwd))
         else:
             response = requests.get(url)
-        result = response.json()['color']
+        return response.json()['color']
 
-        if 'red' in result:
+    def get_color_for_state(self, state):
+        if 'red' in state:
             return 128, 0, 0
-        elif 'blue' in result:
+        elif 'blue' in state:
             return 0, 128, 0
-        elif 'yellow' in result:
+        elif 'yellow' in state:
             return 128, 128, 0
         else:
             return 0, 0, 128
