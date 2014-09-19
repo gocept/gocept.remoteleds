29e967f0fdb3018fd718cd7aa7467f12dc3e7515
oliverz <oliverz@localhost>
2014-09-19 10:21:21 +0200

    Refactor discovery

--- discovery.py
+++ discovery.py
@@ -16,10 +16,7 @@ def main():
     cfg = config.Config()
     cfg.load()
 
-    dev = None
-    while dev is None:
-        dev = discover(cfg.serial_number)
-        time.sleep(SCAN_DELAY_IN_S)
+    dev = discover_loop(cfg.serial_number)
 
     if dev is not None:
         try:
@@ -54,6 +51,13 @@ def main():
         except serial.serialutil.SerialException as e:
             print(e)
 
+
+def discover_loop(snr):
+    dev = None
+    while dev is None:
+        dev = discover(snr)
+        time.sleep(SCAN_DELAY_IN_S)
+    return dev
 
 def discover(serial_number):
     comports = list(serial.tools.list_ports.comports())
